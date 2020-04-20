5348d9bffaddef90c379d2f828719a0d6c38fe6e
Auricdhero <datauric@gmail.com>
2020-04-20 03:22:44 +0000

    made some few changes

--- functions/index.js
+++ functions/index.js
@@ -1,4 +1,21 @@
 const functions = require('firebase-functions');
 const cors = require('cors')({origin: true});
 const admin = require('firebase-admin');
-const serviceAccount = require('./service-account.json')
+const serviceAccount = require('./service-account.json') 
+
+admin.initializeApp({
+    credential: admin.credential.cert(serviceAccount),
+  databaseURL: "https://lucy-enammr.firebaseio.com"
+}); 
+
+const { sessiionsClient } = require('dialogflow');
+
+
+exports.dialogflowGateway = functions.https.onRequest((request, response) => {
+    cors(request, response, async () => {
+        const{ queryInput, sessionId } = request.body;
+
+        const sessiionsClient = new sessiionsClient({ credentials: serviceAccount });
+        const session = sessionClient.sessionPath('')
+    });
+});
